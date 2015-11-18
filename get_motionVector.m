afe4c2a9d8fb3688ce53f3c29b90cd8f6787f811
Krazzyzhall <sshukla5@asu.edu>
2015-11-18 00:35:18 -0700

    Update get_motionVector.m

--- get_motionVector.m
+++ get_motionVector.m
@@ -6,21 +6,25 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [motvector]=get_motionVector(frameset,m)
 %v=VideoReader(filename);
+k=1;
 if(isempty(m))
     m=3;
 end
 [vidHeight,vidWidth]=size(frameset(:,:,1));
-hpart=ceil(vidHeight/3);
-wpart=ceil(vidWidth/3);
-for gi=1:hpart:vidHeight
-    for gj=1:wpart:vidWidth
-        frame1=frameset(gi:gi+hpart,gj:gj+wpart,1);
-        figure,imshow(frame1);
-        frame2=frameset(gi:gi+hpart,gj:gj+wpart,2);
-        figure,imshow(frame1);
-        vect(gi,gj)=get_motionVect_perImg(frame1,frame2);% run for all the grids 
+hpart=floor(vidHeight/3);
+wpart=floor(vidWidth/3);
+for gi=1:hpart:vidHeight-hpart+1
+    for gj=1:wpart:vidWidth-wpart+1
+        gi+hpart
+        gj+wpart
+        frame1=frameset(gi:gi+hpart-1,gj:gj+wpart-1,:,1);
+ %       figure,imshow(frame1);
+        frame2=frameset(gi:gi+hpart-1,gj:gj+wpart-1,:,2);
+%        figure,imshow(frame1);
+        vect(k)={get_motionVect_perImg(frame1,frame2)};% run for all the grids 
+        k=k+1;
     end
 end
 %% convert motion vector into
-motvector=reshape(vect,m*m,1);
+motvector=vect;
 end
