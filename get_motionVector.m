c3a88de743cc387c28341e058c66499c5b270a67
Krazzyzhall <sshukla5@asu.edu>
2015-11-17 23:44:55 -0700

    Update get_motionVector.m
    added code for dividing images into grid and using them for determining motion vector

--- get_motionVector.m
+++ get_motionVector.m
@@ -5,28 +5,22 @@
 %  m        : value of m to create mxm partitions of the image
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [motvector]=get_motionVector(frameset,m)
-v=VideoReader(filename);
+%v=VideoReader(filename);
 if(isempty(m))
     m=3;
 end
 [vidHeight,vidWidth]=size(frameset(:,:,1));
-if(isempty(vstart))
-    vstart=1;
+hpart=ceil(vidHeight/3);
+wpart=ceil(vidWidth/3);
+for gi=1:hpart:vidHeight
+    for gj=1:wpart:vidWidth
+        frame1=frameset(gi:gi+hpart,gj:gj+wpart,1);
+        figure,imshow(frame1);
+        frame2=frameset(gi:gi+hpart,gj:gj+wpart,2);
+        figure,imshow(frame1);
+        vect(gi,gj)=get_motionVect_perImg(frame1,frame2);% run for all the grids 
+    end
 end
-if(isempty(vend))
-    vend=v.Duration.*v.FrameRate;
-end
-s = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),'colormap',[]);
-k=1;
-startf=vstart.*v.FrameRate;
-endf=vend.*v.FrameRate;
-for i=startf:interval:endf
-frames(:,:,:,k)=read(v,i);
-     k=k+1;
- end
-videomat=frames;
-[~,~,~,n]=size(videomat);
-% for i=1:10
-%     figure,imshow(videomat(:,:,:,i));
-% end
+%% convert motion vector into
+motvector=reshape(vect,m*m,1);
 end
