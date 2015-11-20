83b9fd54a1b0105655b8f89300da6a29eaa0f696
jeniljain29392 <jeniljain29392@gmail.com>
2015-11-19 22:17:42 -0700

    Update get_motionVect_perImg.m

--- get_motionVect_perImg.m
+++ get_motionVect_perImg.m
@@ -1,7 +1,7 @@
 function [vect]=get_motionVect_perImg(frame1,frame2,partition)
     [m,n,~]=size(frame1);
     % run algorithm to get optical flow
-    ww= 40;
+    ww= 100;
 w=round(ww/2);
     anchort=im2double(padarray(rgb2gray(frame1),[w w],0));
     targett=im2double(padarray(rgb2gray(frame2),[w w],0));
@@ -13,8 +13,8 @@ tv = zeros(size(anchor));
     I_x=conv2(anchor,[-1 1;-1 1],'valid');      % get partial derivative in x direction
     I_y=conv2(anchor,[-1 -1;1 1],'valid');      % get partial derivative in y direction
     I_t=conv2(anchor,ones(2),'valid') + conv2(target,-ones(2),'valid'); % get partial derivative in t direction   
-    for j=w+1:size(I_x,1)-w
-        for k=w+1:size(I_x,2)-w
+    for j=w+1:2:size(I_x,1)-w
+        for k=w+1:2:size(I_x,2)-w
       Ix = I_x(j-w:j+w, k-w:k+w);
       Iy = I_y(j-w:j+w, k-w:k+w);
       It = I_t(j-w:j+w, k-w:k+w);
@@ -72,6 +72,6 @@ end
 
 % Thresholding - using histogram of motion vectors we find out the 
 %distribution and set a threshold
-% reconst2 = thresholding(targett, u, v);
-% figure,imshow(reconst2,[]); title('Thresholding');
+reconst2 = thresholding(targett, tu, tv);
+figure,imshow(reconst2,[]); title('Thresholding');
 end
