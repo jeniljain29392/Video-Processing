03832d33d0099ff37ee0974e3243eb704fbc7969
Krazzyzhall <sshukla5@asu.edu>
2015-11-19 21:17:32 -0700

    Update get_motionVector.m

--- get_motionVector.m
+++ get_motionVector.m
@@ -11,17 +11,15 @@ if(isempty(m))
     m=3;
 end
 [vidHeight,vidWidth]=size(frameset(:,:,1));
-hpart=floor(vidHeight/3);
-wpart=floor(vidWidth/3);
-for gi=1:hpart:vidHeight-hpart+1
-    for gj=1:wpart:vidWidth-wpart+1
-        gi+hpart
-        gj+wpart
-        frame1=frameset(gi:gi+hpart-1,gj:gj+wpart-1,:,1);
- %       figure,imshow(frame1);
-        frame2=frameset(gi:gi+hpart-1,gj:gj+wpart-1,:,2);
-%        figure,imshow(frame1);
-        vect(k)={get_motionVect_perImg(frame1,frame2)};% run for all the grids 
+hpart=floor(vidHeight/m);
+wpart=floor(vidWidth/m);
+for gi=1:m
+    for gj=1:m
+        frame1=frameset((hpart)*(gi-1)+1:gi*(hpart),(gj-1)*(wpart)+1:gj*(wpart),:,1);
+        figure,imshow(frame1);
+        frame2=frameset((hpart)*(gi-1)+1:gi*(hpart),(gj-1)*(wpart)+1:gj*(wpart),:,2);
+        figure,imshow(frame2);
+        vect(k)={get_motionVect_perImg(frame1,frame2,3)};% run for all the grids 
         k=k+1;
     end
 end
