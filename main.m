93ad9014439c003b630ba81da3701525ee0daaab
jeniljain29392 <jeniljain29392@gmail.com>
2015-11-21 06:42:18 -0700

    Update main.m

--- main.m
+++ main.m
@@ -1,16 +1,37 @@
 clc;
 clear all;
 
-video_04_1 = read_avi_file('rgb-04-1.avi', [], [], 10);
-%load('videomat.mat', 'videomat');
-[ a, b, c, nFrames] = size(video_04_1);
-scaled_vid(:,:,:,1:nFrames) = imresize(video_04_1(:,:,:,1:nFrames), 0.33);
+% video_20_1 = read_avi_file('rgb-20-1.avi', 214, 222, 10);
+% load('video_20_1.mat');
+% [ a, b, c, nFrames] = size(video_20_1);
+%  
+% scaled_vid(:,:,:,1:nFrames) = imresize(video_20_1(:,:,:,1:nFrames), 0.33);
+% 
+% % for i=1:nFrames-1
+% vstart = 214;  vend = 222;
+% strts = (vstart*6); ends = (vend*6);
+% for i = strts:2:ends;
+% %motion_vector = get_motionVector(A(:,:,:,1:2), 3);
+%     vect_20_1(ceil((i-strts+1)/2)) = {get_motionVect_perImg(scaled_vid(:,:,:,i), scaled_vid(:,:,:,i+1), 3)};
+% end
+load('temp_vect1.mat');
 
-for i=1:nFrames-1
-%motion_vector = get_motionVector(A(:,:,:,1:2), 3);
-    vect_04_1(i) = {get_motionVect_perImg(scaled_vid(:,:,:,i), scaled_vid(:,:,:,i+1), 3)};
+[~,nvect] = size(vect_20_1);
+% mean([1;2;3])
+% vect_20_1{2}(2,2)
+% vect_20_1{2}
+for k=1:3:nvect-3
+    for i=1:3
+        for j=1:3
+            1s_vect{ceil(k/3)}(i,j) = mean([vect_20_1{k}(i,j);
+                                     vect_20_1{k+1}(i,j);
+                                     vect_20_1{k+2}(i,j)]);
+        end
+    end
 end
-pause;
-save video_04_1.mat video_04_1
-save vect_04_1.mat vect_04_1;
+
+%pause;
+%save temp_vect1 vect_20_1;
+%save video_20_1.mat video_20_1;
+%save vect_20_1.mat vect_20_1;
 close all;
