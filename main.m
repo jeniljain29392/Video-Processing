bcacbee117792b106ab183cdd24149c7134626f2
jayaram theegala <theegalajayaram@gmail.com>
2015-11-21 01:56:35 -0700

    Update main.m

--- main.m
+++ main.m
@@ -1,16 +1,15 @@
 clc;
 clear all;
 
-%videomat = read_avi_file('rgb-04-1.avi', 145.5, 148, 10);
-load('videomat.mat', 'videomat');
-[ a, b, c, nFrames] = size(videomat);
-A(:,:,:,1:nFrames) = imresize(videomat(:,:,:,1:nFrames), 0.33);
+video_04_1 = read_avi_file('rgb-04-1.avi', [], [], 10);
+%load('videomat.mat', 'videomat');
+[ a, b, c, nFrames] = size(video_04_1);
+scaled_vid(:,:,:,1:nFrames) = imresize(video_04_1(:,:,:,1:nFrames), 0.33);
 
 for i=1:nFrames-1
 %motion_vector = get_motionVector(A(:,:,:,1:2), 3);
-    vect = get_motionVect_perImg(A(:,:,:,i), A(:,:,:,i+1), 3);
-    pause;
+    vect_04_1(i) = {get_motionVect_perImg(scaled_vid(:,:,:,i), scaled_vid(:,:,:,i+1), 3)};
 end
 pause;
-%save videomat.mat videomat;
+save hash_04_1.mat video_04_1 vect_04_1;
 close all;
