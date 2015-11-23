1bb8df7f00e90e871f43d30689b5e40dc740d161
jeniljain29392 <jeniljain29392@gmail.com>
2015-11-22 18:20:21 -0700

    Update main.m

--- main.m
+++ main.m
@@ -2,34 +2,44 @@ clc;
 clear all;
 
 % video_20_1 = read_avi_file('rgb-20-1.avi', 214, 222, 10);
-% load('video_20_1.mat');
-% [ a, b, c, nFrames] = size(video_20_1);
+load('video_25_1.mat');
+% [~,~,~,nFrames] = size(video_25_1);
 %  
-% scaled_vid(:,:,:,1:nFrames) = imresize(video_20_1(:,:,:,1:nFrames), 0.33);
-% 
-% % for i=1:nFrames-1
-% vstart = 214;  vend = 222;
-% strts = (vstart*6); ends = (vend*6);
-% for i = strts:2:ends;
+% scaled_vid(:,:,:,1:nFrames) = imresize(video_25_1(:,:,:,1:nFrames), 0.33);
+
+for i=1:2:nFrames-1
+% % vstart = 214;  vend = 222;
+% % strts = (vstart*6); ends = (vend*6);
+% % for i = strts:2:ends;
 % %motion_vector = get_motionVector(A(:,:,:,1:2), 3);
-%     vect_20_1(ceil((i-strts+1)/2)) = {get_motionVect_perImg(scaled_vid(:,:,:,i), scaled_vid(:,:,:,i+1), 3)};
+%     i
+%     vect_25_1(ceil(i/2)) = {get_motionVect_perImg(scaled_vid(:,:,:,i), scaled_vid(:,:,:,i+1), 3)};
 % end
-load('temp_vect1.mat');
+% load('temp_vect1.mat');
 
-[~,nvect] = size(vect_20_1);
-% mean([1;2;3])
-% vect_20_1{2}(2,2)
-% vect_20_1{2}
-for k=1:3:nvect-3
-    for i=1:3
-        for j=1:3
-            vect_1s{ceil(k/3)}(i,j) = mean([vect_20_1{k}(i,j);
-                                     vect_20_1{k+1}(i,j);
-                                     vect_20_1{k+2}(i,j)]);
-        end
-    end
-end
+% [~,nvect] = size(vect_25_1);
+% % mean([1;2;3])
+% % vect_20_1{2}(2,2)
+% % vect_20_1{2}
+% for k=1:3:nvect-3
+%     for i=1:3
+%         for j=1:3
+%             vect_1s{ceil(k/3)}(i,j) = mean([vect_25_1{k}(i,j);
+%                                      vect_25_1{k+1}(i,j);
+%                                      vect_25_1{k+2}(i,j)]);
+%         end
+%     end
+% end
 
+load('vect_25_1_sec.mat');
+
+% [~,n] = size(vect_1s);
+% for i=1:n
+%    resized_vect(i) = {reshape(vect_1s{i}, [1,9])};
+%    feat_vect(i,:) = resized_vect{i};
+% end
+% 
+% IDX = kmeans(feat_vect, 4);
 %pause;
 %save temp_vect1 vect_20_1;
 %save video_20_1.mat video_20_1;
