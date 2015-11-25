5659e7d0973e4542dac76ba0182b6eca994fd04e
jeniljain29392 <jeniljain29392@gmail.com>
2015-11-24 22:45:33 -0700

    Update training.m

--- training.m
+++ training.m
@@ -1,18 +1,24 @@
 clc;
 clear all;
-files = dir('dataset/*.avi');
+files = dir('*.avi');
 motion_vector_arr = struct();
 motion_vector_arr.name = '';
 motion_vector.motionVectors = [];
-i = 1;
+j = 1;
 for file = files'
-    videomat = read_avi_file(strcat('dataset/',file.name), 145, 146, 5);
-    motion_vector = get_motionVector(videomat(:,:,:,1:2), 3);
-    motion_vector_arr(i).name = file.name;
-    motion_vector_arr(i).motionVectors = motion_vector;
-    i=i+1;
+    videomat = read_avi_file(file.name, [], [], 5);
+    [~,~,~,nFrames] = size(videomat);
+    vectmat = repmat(cell(1), 1, nFrames);
+    
+    for i=1:2:2
+        vectmat(ceil(i/2)) = {get_motionVect_celiu(videomat(:,:,:,i)...
+        ,videomat(:,:,:,i+1))};
+    end
+    
+    motion_vector_arr(j).name = file.name;
+    motion_vector_arr(j).motionVectors = vectmat;
+    j=j+1;
 end
 %get_motionVect_perImg(videomat(:,:,:,1), videomat(:,:,:,2));
 save motion_vector_arr.mat motion_vector_arr;
-pause; 
 close all;
