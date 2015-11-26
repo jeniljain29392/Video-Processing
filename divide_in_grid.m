20a63ef4b96b424e0c7f475da8a89fb131f5660e
jeniljain29392 <jeniljain29392@gmail.com>
2015-11-25 18:07:15 -0700

    Update divide_in_grid.m

--- divide_in_grid.m
+++ divide_in_grid.m
@@ -1,11 +1,16 @@
-%% divides images int mxm images and removes w rows and columns from parameter image
-function [vect]=divide_in_grid(image,m,w)
-temp=image(w+1:end-w,w+1:end-w);
-[p,n] = size(temp);
-pheight = floor(p/m);
-pwidth = floor(n/m);
-for i=1:m
-    for j=1:m
-        vect(i,j)=sum(sum(temp((pheight)*(i-1)+1:pheight*i,(pwidth)*(j-1) + 1:j*pwidth)));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  divides images int mxm images 
+%  image : Frame to be divided into grid
+%  w = window size of the grid
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [vect] = divide_in_grid(image,w)
+[n,m] = size(image);
+ht_w = floor(n/w);      %height after dividing image into mxm grid
+wt_w = floor(m/w);      %weight after dividing image into mxm grid
+for i=1:w
+    for j=1:w
+        vect(i,j) = sum(sum(image((ht_w)*(i-1)+1:i*ht_w,...
+                                  (wt_w)*(j-1)+1:j*wt_w)));
     end
 end
