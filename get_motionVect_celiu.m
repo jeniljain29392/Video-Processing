0df3ac8d8e99938a978d7a59ff3a7199d928ffca
jeniljain29392 <jeniljain29392@gmail.com>
2015-11-29 05:54:50 -0700

    Update get_motionVect_celiu.m

--- get_motionVect_celiu.m
+++ get_motionVect_celiu.m
@@ -7,11 +7,19 @@
 
 function [reconst]=get_motionVect_celiu(frame1,frame2)
 
+alpha = 0.10;
+ratio = 0.5;
+minWidth = 20;
+nOuterFPIterations = 3;
+nInnerFPIterations = 1;
+nSORIterations = 10;
+para = [alpha,ratio,minWidth,nOuterFPIterations,nInnerFPIterations,nSORIterations];
+
 anchor=rgb2gray(frame1);
 target=rgb2gray(frame2);
 
 %% perform motion estimation
-[u,v,~] = Coarse2FineTwoFrames(anchor,target);
+[u,v,~] = Coarse2FineTwoFrames(anchor,target, para);
 
 %% perform segmentation using thresholding
 reconst = thresholding(target, u, v);
