221657d9d98eed65a09af5af19c97dfc8bc270d1
jeniljain29392 <jeniljain29392@gmail.com>
2015-11-30 03:04:37 -0700

    Update training.m

--- training.m
+++ training.m
@@ -5,28 +5,28 @@
 %   - save vectors into the mat file
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  this file 
+%   - read all the Video files in the dataset
+%   - perform motion estimation and segmentation
+%   - save vectors into the mat file
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 clc;
 clear all;
-files = dir('*.avi');
 
-j = 1;
-for file = files'
-    
-    % Perform Motion estimation
-    videomat = read_avi_file(file.name, [], [], 5);
-    %load(' video_18_02.mat');
-    [~,~,~,nFrames] = size(videomat);
-    vectmat = repmat(cell(1), 1, nFrames);
-    
-    % Perform motion segmentation
-    for i=1:2:nFrames-1
-        i
-        vectmat(ceil(i/2)) = {get_motionVect_celiu(videomat(:,:,:,i)...
-        ,videomat(:,:,:,i+1))};
-    end
+% Perform Motion estimation. Change file name
+videomat = read_avi_file('rgb-02-1.avi', [], [], 5);
+%load(' video_18_02.mat');
+[~,~,~,nFrames] = size(videomat);
+vectmat = repmat(cell(1), 1, nFrames);
 
-    j=j+1;
+% Perform motion segmentation
+for i=1:2:nFrames-1
+    i
+    vectmat(ceil(i/2)) = {get_motionVect_celiu(videomat(:,:,:,i)...
+    ,videomat(:,:,:,i+1))};
 end
-
-save -v7.3 vectmat_17_02.mat vectmat;
+    
+save -v7.3 vectmat_02_01.mat vectmat;
 close all;
