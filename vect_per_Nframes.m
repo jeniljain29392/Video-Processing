b72f290dd16bc54647e80abad653690b84c9fcaa
jeniljain29392 <jeniljain29392@gmail.com>
2015-12-01 01:31:40 -0700

    Update vect_per_Nframes.m

--- vect_per_Nframes.m
+++ vect_per_Nframes.m
@@ -4,8 +4,8 @@ function [vect_per_Nframes] = vect_per_Nframes(vect, n)
 if(isempty(n))
     for i = 1:nFrames
         reshaped_vect(i,:) = reshape(vect{i}, [1, row*col]);
-        vect_per_Nframes = reshape(mean(reshaped_vect), [row, col]);
     end
+    vect_per_Nframes = reshape(mean(reshaped_vect), [row, col]);
 else
     for i = 1:n:nFrames-n
         for j = 0:n-1
