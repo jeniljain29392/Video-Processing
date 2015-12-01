bce9c10a02058ad2a182d24eff934cca1288f4c9
jeniljain29392 <jeniljain29392@gmail.com>
2015-11-30 19:33:33 -0700

    Update cosine_distance.m

--- cosine_distance.m
+++ cosine_distance.m
@@ -1,10 +1,7 @@
 function [cosine_dist] = cosine_distance(input_vect, output_vect)
-for j = 1:3
-    for k = 1:3
-        prod = output_vect(j,k).*input_vect(j,k);
-        abs_out = sum(sum(output_vect(j,k).*output_vect(j,k)));
-        abs_in = sum(sum(input_vect(j,k).*input_vect(j,k)));
-        cosine_dist = 1 - (prod/(abs_out+abs_in));
-    end
+prod = sum(sum(output_vect.*input_vect));
+abs_out = sum(sum(output_vect.*output_vect));
+abs_in = sum(sum(input_vect.*input_vect));
+cosine_dist = 1 - (prod/(abs_out .* abs_in));
 end
-end
+
